299da0c5a1b8a1441d1c44cd92289f3ac97cd428
cpulover <justacpulover@gmail.com>
2020-07-12 06:57:19 +0200

    Lombok annotations

--- README.md
+++ README.md
@@ -5,14 +5,24 @@
 - [Jar/Maven](https://search.maven.org/search?q=g:org.projectlombok%20AND%20a:lombok&core=gav)
 - [Features](https://projectlombok.org/features/all)
 - [Implementation](https://github.com/peichhorn/lombok-pg)
-#### Annotations [[TomBokDriver]()]
-- @Log
-- @Data
-- @AllArgsConstructor
-- @NoArgsConstructor
-- @ToString
-- @Getter and @Setter
-- @Builder
+
+#### Basic annotations [[LombokDriver]()]
+- [@Log](https://projectlombok.org/features/log)
+- [@AllArgsConstructor - @NoArgsConstructor](https://projectlombok.org/features/constructor)
+- [@RequiredArgsConstructor](@NoArgsConstructor)
+: for final or non-null attributes
+- [@ToString](https://projectlombok.org/features/ToString)
+- [@Getter - @Setter](https://projectlombok.org/features/GetterSetter)
+- [@Setter(AccessLevel.PROTECTED/PUBLIC/PACKAGE/PRIVATE)](https://projectlombok.org/features/GetterSetter)
+- [@Builder](https://projectlombok.org/features/Builder)
+- [@NonNull](https://projectlombok.org/features/NonNull)
+- [@Cleanup](https://projectlombok.org/features/Cleanup)
+: call close() methods
+
+
+#### Combining annotations
+- [@Data](https://projectlombok.org/features/Data) 
+=  @ToString + @EqualsAndHashCode + @Getter + @Setter + @RequiredArgsConstructor
 
 
 
--- src/main/java/com/cpulover/justa/lombok/LombokDriver.java
+++ src/main/java/com/cpulover/justa/lombok/LombokDriver.java
@@ -1,14 +1,18 @@
-package com.cpulover.justa.tombok;
+package com.cpulover.justa.lombok;
+
+import java.util.Scanner;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Cleanup;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.ToString;
 import lombok.extern.java.Log;
 
 @Log // or: @Slf4j @CommonsLog @Log4j @Log4j2 @XSlf4j
-public class TombokDriver {
+public class LombokDriver {
 	public static void main(String[] args) {
 		log.info("Start program"); // @Log
 		Person person1 = new Person("Cpulover", 20); // @AllArgsConstructor
@@ -16,11 +20,24 @@ public class TombokDriver {
 		person1.setName("Justa"); // @Data
 		person1.setAge(20); // @Data
 		System.out.println(person1); // @Data
-		
-		log.severe("St is wrong here... JK!");
+
+		log.severe("S.t is wrong here... JK!"); // @Log
 
 		Address address1 = Address.builder().streetNumber(13).streetName("AAA").build(); // @Builder
 		System.out.println(address1); // @ToString
+
+		@Cleanup Scanner input = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = input.nextLine();
+		System.out.println("Hi " + name);
+
+		Person personNull = null;
+		checkNonNull(person1); // @NotNull
+		checkNonNull(personNull); // @NotNull
+	}
+
+	public static void checkNonNull(@NonNull Person person) {
+		System.out.println("Checking non-null: " + person);
 	}
 }
 
